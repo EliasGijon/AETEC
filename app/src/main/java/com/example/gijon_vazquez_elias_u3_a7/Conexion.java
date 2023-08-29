e9eab22ec923995ddf5f2dc0fb7bf1679271ad9c
elias.gijon <saske935@hotmail.com>
2023-08-29 02:23:16 -0500

    code

---
+++ app/src/main/java/com/example/gijon_vazquez_elias_u3_a7/Conexion.java
@@ -0,0 +1,30 @@
+package com.example.gijon_vazquez_elias_u3_a7;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static String url =  "jdbc:sqlserver://zc.database.windows.net:1433;databaseName=tienda_virtual";
+    private static String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";//com.microsoft.sqlserver.jdbc.SQLServerDriver
+    //com.mysql.jdbc.Driver
+    private static String username = "zckeeper";
+    private static String password = "SecurityBad21";
+    private static Connection con=null;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(driverName);
+            try {
+                con = DriverManager.getConnection(url, username, password);
+            } catch (SQLException ex) {
+                // log an exception. fro example:
+                System.out.println("Failed to create the database connection.");
+            }
+        } catch (ClassNotFoundException ex) {
+            // log an exception. for example:
+            System.out.println("Driver not found.");
+        }
+        return con;
+    }
+}
